5538a41029876d42a58c94161b4adb01a3e97d91
Ryan Richard <klaethecoder@gmail.com>
2019-03-05 23:10:36 -0700

    I have gotten the app to encrypt a message that the user puts in by a cipher key that the user gives. It then prints out the message for the user to see.

--- ceasarCipher.py
+++ ceasarCipher.py
@@ -1,15 +1,19 @@
 from string import ascii_lowercase as lowercase
 
-alphabet = lowercase
-message = input("Please enter a message you want to be encrypted \n")
-newMessage = ""
+usersKey = int(input("What cipher key would you like? (Pick a number 1-26)"))
+def cipherEncryption(key):
+    alphabet = lowercase
+    message = input("Please enter a message you want to be encrypted \n").lower()
+    newMessage = ""
 
-for letter in message:
-    if letter in alphabet:
-        position = alphabet.find(letter)
-        newPosition = (position + 13) % 26
-        newMessage += alphabet[newPosition]
-    else:
-        newMessage += letter
+    for letter in message:
+        if letter in alphabet:
+            position = alphabet.find(letter)
+            newPosition = (position + key) % 26
+            newMessage += alphabet[newPosition]
+        else:
+            newMessage += letter
 
-print(newMessage)
+    print(newMessage)
+
+cipherEncryption(usersKey-1)
