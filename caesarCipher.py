66e57e281cf2c86260cdde6041a45df089abef38
Ryan Richard <klaethecoder@gmail.com>
2019-03-06 22:52:44 -0700

    I have created a decoder function that will help with the decoding of messages when as the user copys it into the prompt

--- caesarCipher.py
+++ caesarCipher.py
@@ -16,9 +16,25 @@ def cipherEncryption(key):
 
     print(newMessage)
 
-try:
-    usersKey = int(input("What cipher key would you like? (Pick a number 1-26)"))
-    cipherEncryption(usersKey-1)
+# try:
+#     usersKey = int(input("What cipher key would you like? (Pick a number 1-26)"))
+#     cipherEncryption(usersKey-1)
 
-except ValueError:
-    print("Please choose a number between 1-26")
+# except ValueError:
+#     print("Please choose a number between 1-26")
+
+def decoderRing(message,key):
+    alphabet = lowercase
+    newMessage = ""
+    for letter in message:
+        if letter in alphabet:
+            position = alphabet.find(letter)
+            newPosition = (position - key) % 26
+            newMessage += alphabet[newPosition]
+        else:
+            newMessage += letter
+    print(newMessage)
+
+stuff = input("What is kind of message do you need to decode? \n")
+key = int(input("What is the key that you used? \n"))-1
+decoderRing(stuff, key)
