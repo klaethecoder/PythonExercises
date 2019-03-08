249e9c600b5b1aa8da45757e9529f17e0bf63677
Ryan Richard <klaethecoder@gmail.com>
2019-03-07 22:36:44 -0700

    Created another function to draw another shape within a window.

--- UdacityCourses/turtleDraw.py
+++ UdacityCourses/turtleDraw.py
@@ -1,34 +1,44 @@
 import turtle
+from turtle import *
+
+def drawSquare(turtle):
+    counter = 1
+    while counter <=4:
+        turtle.forward(100)
+        turtle.right(90)
+        counter+= 1
 
 def drawing():
     window = turtle.Screen()
     window.bgcolor("red")
     brad = turtle.Turtle()
-    angie = turtle.Turtle()
-    fred = turtle.Turtle()
+    brad.speed(15)
+    # angie = turtle.Turtle()
+    # fred = turtle.Turtle()
     brad.shape("turtle")
-    angie.shape("turtle")
-    fred.shape("turtle")
-    brad.color("green")
-    angie.color("yellow")
-    fred.color("blue")
-    counter = 1
-    angie.circle(200)
-    fred.circle(100)
-    while counter <=4:
-        brad.forward(100)
-        brad.right(90)
-        counter+= 1
+    # angie.shape("turtle")
+    # fred.shape("turtle")
+    brad.color("yellow")
+    # angie.color("yellow")
+    # fred.color("blue")
+    for i in range(1,37):
+        drawSquare(brad)
+        brad.right(10)
+
     window.exitonclick()
 
+def starShape():
+    Screen()
+    speed(20)
+    color('red', 'yellow')
+    begin_fill()
+    while True:
+        forward(200)
+        left(170)
+        if abs(pos()) < 1:
+            break
+    end_fill()
+    exitonclick()
+
 drawing()
-
-# def draw():
-#      window = turtle.Screen()
-#      window.bgcolor("red")
-#      brad = turtle.Turtle()
-#      turtle.begin_fill()
-#      brad.forward(100)
-
-# # print(dir(turtle))
-# draw()
+# starShape()
