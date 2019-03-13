f18e6d0bcb18a56f69d3a20b8620df86cc5d2ebe
Ryan Richard <klaethecoder@gmail.com>
2019-03-13 05:47:01 -0600

    This is the start of a movie class app that will display information about a movie.

---
+++ UdacityCourses/movies.py
@@ -0,0 +1,9 @@
+import webbrowser, omdb
+
+class Movie():
+    def __init__(self, title, storyLine, poster, trailer):
+        self.title = title
+        self.poster = poster
+        self.storyLine = storyLine
+        self.trailer = trailer
+        
