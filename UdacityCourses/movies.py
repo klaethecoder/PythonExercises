153d3875446e04d994c30df7ffcd9508e81bcf83
Ryan Richard <klaethecoder@gmail.com>
2019-03-14 20:39:36 -0600

    Updated the movie file to add a couple more features from the Fresh Tomatoes file.

--- UdacityCourses/movies.py
+++ UdacityCourses/movies.py
@@ -1,4 +1,5 @@
-import webbrowser, omdb
+import webbrowser, omdb 
+import fresh_tomatoes as ft
 
 class Movie():
     def __init__(self, title, storyLine, poster, trailer):
@@ -7,11 +8,15 @@ class Movie():
         self.storyLine = storyLine
         self.trailer = trailer
 
-
-def showTrailer(trailer):
-            webbrowser.open(trailer)
+    def showTrailer(self):
+            webbrowser.open(self.trailer)
         
-toyStory = Movie("Toy Story", "A cowboy doll is profoundly threatened and jealous when a new spaceman figure supplants him as top toy in a boy's room.", "https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_SY1000_SX670_AL_.jpg", "https://www.imdb.com/title/tt0114709/videoplayer/vi2052129305?ref_=tt_ov_vi")
+toyStory = Movie("Toy Story", "A cowboy doll is profoundly threatened and jealous when a new spaceman figure supplants him as top toy in a boy's room.", "https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_SY1000_SX670_AL_.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-print(toyStory.title)
-showTrailer(toyStory.trailer)
+schoolOfRock = Movie("School of Rock", "After being kicked out of his rock band, Dewey Finn becomes a substitute teacher of an uptight elementary private school, only to try and turn them into a rock band.", "https://m.media-amazon.com/images/M/MV5BMjEwOTMzNjYzMl5BMl5BanBnXkFtZTcwNjczMTQyMQ@@._V1_.jpg", "https://www.youtube.com/watch?v=XCwy6lW5Ixc")
+
+# print(toyStory.title)
+# toyStory.showTrailer()
+
+movies = [toyStory, schoolOfRock]
+ft.open_movies_page(movies)
