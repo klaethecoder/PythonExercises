19bf436a5b4b89753cdab4834a4198f0bd21ad23
Ryan Richard <klaethecoder@gmail.com>
2019-03-14 19:34:05 -0600

    I added more to the class, an instance of the class and added a function to open a browser window to play the movie trailer

--- UdacityCourses/movies.py
+++ UdacityCourses/movies.py
@@ -6,4 +6,12 @@ class Movie():
         self.poster = poster
         self.storyLine = storyLine
         self.trailer = trailer
+
+
+def showTrailer(trailer):
+            webbrowser.open(trailer)
         
+toyStory = Movie("Toy Story", "A cowboy doll is profoundly threatened and jealous when a new spaceman figure supplants him as top toy in a boy's room.", "https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_SY1000_SX670_AL_.jpg", "https://www.imdb.com/title/tt0114709/videoplayer/vi2052129305?ref_=tt_ov_vi")
+
+print(toyStory.title)
+showTrailer(toyStory.trailer)
