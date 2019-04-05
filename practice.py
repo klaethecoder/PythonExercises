cc69e5875a98a7d9f599e65e0c182299e01f012b
Ryan Richard <klaethecoder@gmail.com>
2019-04-05 06:32:08 -0600

    Made some changes to the file to refactor some of the code

--- practice.py
+++ practice.py
@@ -102,3 +102,19 @@ e,f = f,e
 
 print(e)
 print(f)
+
+# Given the tuple below that represents the Imelda May album "More Mayhem", write
+# code to print the album details, followed by a listing of all the tracks in the album.
+#
+# Indent the tracks by a single tab stop when printing them (remember that you can pass
+# more than one item to the print function, separating them with a comma).
+
+imelda = "More Mayhem", "Imelda May", 2011, (
+    (1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"), (4, "Kentish Town Waltz"))
+#
+# print(imelda)
+
+title, artist, year, tracks = imelda
+print("Title: {} | Artist: {} | Release Year: {}".format(title, artist, year))
+for song in tracks:
+    print("\t",song)
